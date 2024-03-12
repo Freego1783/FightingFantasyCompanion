34967e166cb1f8e0501af2d7a4dfccd2faf361d5
Hugo Pernet <hugo.pernet@esial.net>
2024-03-12 21:51:46 +0100

    Added Blazored.LocalStorage and some stuff to test it out

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using FightingFantasyCompanion;
 using MudBlazor.Services;
+using Blazored.LocalStorage;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,5 +14,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddMudServices();
+builder.Services.AddBlazoredLocalStorage(config => config.JsonSerializerOptions.Encoder = JavaScriptEncoder.Create(UnicodeRanges.All));
 
 await builder.Build().RunAsync();
---
+++ Shared/Models/AdventureSheet.cs
@@ -0,0 +1,23 @@
+﻿using FightingFantasyCompanion.Shared.Enums;
+
+namespace FightingFantasyCompanion.Shared.Models
+{
+    public class AdventureSheet
+    {
+        public string Id { get; set; }
+        public BookName Bookname { get;set; }
+        public int InitialSkill { get; set; }
+        public int Skill { get; set; }
+        public int InitialStamina { get; set; }
+        public int Stamina { get; set;}
+        public int InitialLuck { get; set; }
+        public int Luck { get; set; }
+        public int Gold { get;set; }
+        public int Provisions { get; set; }
+
+        public AdventureSheet()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+    }
+}
